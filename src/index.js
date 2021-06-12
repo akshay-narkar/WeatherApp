17dbcd0af8da94da1d036bf20f7e5e25b7c41c83
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-12 21:56:42 +0530

    Change import bootstrap link

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import 'bootstrap';
+// import 'bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import './stylesheet.scss';
 import { start } from './logic';
 
