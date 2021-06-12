d5490d104e0b733fcaf773f24fcfbc6384f7172c
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-12 22:19:29 +0530

    Update index file and remove import bs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import 'bootstrap';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import './stylesheet.scss';
 import { start } from './logic';
