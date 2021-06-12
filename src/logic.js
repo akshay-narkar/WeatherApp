913dc995d696443b61a95643e2bdbc070a368f4c
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-12 20:07:26 +0530

    Add async function along with functions for displaying data

--- src/logic.js
+++ src/logic.js
@@ -1,39 +1,40 @@
-import {displaydata} from './dom';
+import { nodata, displaydata, cityname } from './dom';
 
-const key = 'ee6f65e9ba3eb85072e21ce3e513cac0';
-let json1 = 0  ; // const random = "Akshay"; // important.. why doenst it show in console?
+const key = 'API_KEY_HERE';
+export const weatherdata = document.getElementById('weatherdata');
+let json1 = 0; // const random = "Akshay"; // important.. why doenst it show in console?
 
-export function start() {
+async function findtemp(e = false, location) {
+  e.preventDefault();
+  cityname.textContent = 'Fetching Data....';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
 
-const submitweather = document.querySelector('#formforweather');
-// const weatherdata = document.querySelector('#weatherdata');
-// const random = "Akshay"; // important.. why doenst it show in console?
+  const response = await fetch(url);
+  // if HTTP-status is 200-299
+  // get the response body (the method explained below)
+  if (response.ok) {
+    json1 = await response.json();
+    const { icon } = json1.weather[0];
+    const iconurl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    const jsonimage = await fetch(iconurl);
+    if (jsonimage.ok) {
+      displaydata(json1, jsonimage.url);
+    } else {
+      nodata('City not found');
 
-submitweather.addEventListener('submit',(e)=>{findtemp(e,weatherdata.value)});
+      //   alert(`HTTP-Error: ${response.status}`);
+    }
+  } else {
+    nodata('City not found');
 
+    // alert("HTTP-Error: " + response.status);
+  }
 }
 
-async function findtemp(e=false,location){
-    e.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
+export function start() {
+  const submitweather = document.querySelector('#formforweather');
+  // const weatherdata = document.querySelector('#weatherdata');
+  // const random = "Akshay"; // important.. why doenst it show in console?
 
-    let response = await fetch(url)
-    // if HTTP-status is 200-299
-    // get the response body (the method explained below)
-    if (response.ok) { 
-                json1 = await response.json();
-                let icon = json1.weather[0].icon
-                let iconurl = `http://openweathermap.org/img/wn/${icon}@2x.png`
-                let jsonimage = await fetch(iconurl);
-                if (jsonimage.ok) {   
-                        displaydata(json1,jsonimage.url);
-                    }
-                else {
-                    alert("HTTP-Error: " + response.status);
-                }
-    } 
-    
-    else {
-        alert("HTTP-Error: " + response.status);
-    }
+  submitweather.addEventListener('submit', (e) => { findtemp(e, weatherdata.value); });
 }
