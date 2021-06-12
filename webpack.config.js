287623795353a89f804b1bb31867003eefd1d418
akshay-narkar <akshaynarkar1295@gmail.com>
2021-06-13 02:00:37 +0530

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -16,15 +16,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
+
+        test: /\.css$/,
         use: [
-          // Creates `style` nodes from JS strings
           'style-loader',
-          // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
         ],
+        // test: /\.s[ac]ss$/i,
+        // use: [
+        //   // Creates `style` nodes from JS strings
+        //   'style-loader',
+        //   // Translates CSS into CommonJS
+        //   'css-loader',
+        //   // Compiles Sass to CSS
+        //   'sass-loader',
+        // ],
       },
 
       {
